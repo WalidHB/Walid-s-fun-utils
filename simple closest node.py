94b4cdfe034c73ccf8c79e32c72f5968fbfe16ab
WalidHB <77410694+WalidHB@users.noreply.github.com>
2024-07-21 23:08:22 +0300

    Create simple closest node.py

---
+++ simple closest node.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def closest_node(node, nodes):
+    nodes = np.asarray(nodes)
+    deltas = nodes - node
+    dist_2 = np.einsum('ij,ij->i', deltas, deltas)
+    return nodes[np.argmin(dist_2)]
