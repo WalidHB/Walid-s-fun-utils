e935b78ff2dddaeee6492269936ae04ff15c09e5
WalidHB <77410694+WalidHB@users.noreply.github.com>
2024-07-21 23:09:31 +0300

    Update and rename internet.py to internet check.py

--- internet check.py
+++ internet check.py
@@ -12,7 +12,8 @@ def internet_check(url = "https://www.google.com/"):
 while True:
     time.sleep(5)
     if not internet_check():
-        print("trying")
+        
+        print("trying to restart adabter")
         p1 = subprocess.Popen('netsh interface set interface "Wi-Fi" disable')
         p1.wait()
         p1.kill()
