537579b03e560649d759c080e00f92d967d53aac
WalidHB <77410694+WalidHB@users.noreply.github.com>
2024-07-21 23:09:05 +0300

    Add files via upload

---
+++ internet.py
@@ -0,0 +1,22 @@
+import requests
+import time
+import subprocess
+
+def internet_check(url = "https://www.google.com/"):
+    try:
+        requests.get(url)
+        return True
+    except:
+        return False
+
+while True:
+    time.sleep(5)
+    if not internet_check():
+        print("trying")
+        p1 = subprocess.Popen('netsh interface set interface "Wi-Fi" disable')
+        p1.wait()
+        p1.kill()
+        p2 = subprocess.Popen('netsh interface set interface "Wi-Fi" enable')
+        p2.wait()
+        p2.kill()
+    print('not yet', time.time())
